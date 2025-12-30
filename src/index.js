5489b3bb0148b3610c3d6941bedead4a7d46ef5a
aryaman-applify <aryaman.j@applify.co>
2025-12-30 13:50:31 +0530

    real code-build output to be shown by ec2 which has appropriate permission to read from s3

---
+++ src/index.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  root.innerHTML = "<h1>Hello Aryaman!</h1><p>This is built with Webpack and deployed via CodeBuild → EC2 → Nginx.</p>";
+});
